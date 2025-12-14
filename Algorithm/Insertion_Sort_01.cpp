6c1914008d2186d98586f50e2b74a1c049f90370
ArpanParajuli <arpanparajuli394@gmail.com>
2025-12-14 10:16:50 +0545

    Basic insertion sort

---
+++ Algorithm/Insertion_Sort_01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+
+
+int main(){
+
+
+    int arr[] = {12, 11, 13, 5, 6};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+   for(int i = 1; i < n; i++){
+   int temp = arr[i];
+   int j = i;
+
+
+   while(j > 0 && arr[j -1] > temp){
+       arr[j] = arr[j -1];
+       j--; 
+   }
+
+   arr[j] = temp;
+
+}
+
+
+
+    for(int i = 0; i < n; i++){
+        std::cout << arr[i] << " ";
+    }    return 0;
+}
